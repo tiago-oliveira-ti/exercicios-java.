ee18191da2a785a214b7308f5095a1354d10a513
Tiago Oliveira <tiago.oliveira.tech@outlook.com>
2026-06-14 18:12:55 +0000

    Adiciona exercício Pares/Ímpares em ordem decrescente

--- ex03-pares-impares/ParesImpares.java
+++ ex03-pares-impares/ParesImpares.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class ParesImpares {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int inicio = sc.nextInt();
+
+        System.out.print("Digite o segundo número (maior que o primeiro): ");
+        int fim = sc.nextInt();
+
+        System.out.println("Escolha: 1 - Pares | 2 - Ímpares");
+        int opcao = sc.nextInt();
+
+        System.out.println("\nResultado em ordem decrescente:");
+        for (int i = fim; i >= inicio; i--) {
+            if (opcao == 1 && i % 2 == 0) {
+                System.out.println(i);
+            } else if (opcao == 2 && i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}

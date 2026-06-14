4745143d674451df8a9055d2d36e0484e7e221ac
Tiago Oliveira <tiago.oliveira.tech@outlook.com>
2026-06-14 13:55:48 +0000

    Adiciona tabuada com loop for

--- ex01-tabuada/Tabuada.java
+++ ex01-tabuada/Tabuada.java
@@ -4,15 +4,17 @@ public class Tabuada {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite um número para ver a tabuada: ");
+        System.out.print("Digite um número: ");
         int numero = sc.nextInt();
 
 
-        System.out.println("Tabuada do " + numero + ":");
+        System.out.println("\nTabuada do " + numero + ":");
         for (int i = 1; i <= 10; i++) {
-            
-            System.out.println(numero + " x " + i + " = " + (numero *
 
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        sc.close();
 
 
        }

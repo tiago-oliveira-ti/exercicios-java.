20f124c477c924faf78dbd531ebdb8756fb505c3
Tiago Oliveira <tiago.oliveira.tech@outlook.com>
2026-06-16 14:30:38 +0000

    Implementação da tabuada com código organizado

--- src/ex01_tabuada/Tabuada.java
+++ src/ex01_tabuada/Tabuada.java
@@ -1,26 +1,19 @@
 package ex01_tabuada;
 
-
 import java.util.Scanner;
 
 public class Tabuada {
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite um número: ");
         int numero = sc.nextInt();
 
-
         System.out.println("\nTabuada do " + numero + ":");
         for (int i = 1; i <= 10; i++) {
-
             System.out.println(numero + " x " + i + " = " + (numero * i));
         }
 
         sc.close();
-
-
-       }
-
-
+    }
 }
